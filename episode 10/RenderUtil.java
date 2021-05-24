0f88b111c60bbe1255a169b5c8d9c8ddbef68248
quickdaffy <71281295+quickdaffy@users.noreply.github.com>
2021-05-24 15:23:10 -0400

    Delete episode 10 directory

--- episode 10/RenderUtil.java
+++
@@ -1,9 +0,0 @@
- public static void drawHollowRect(int x, int y, int w, int h, int color) {
-
-      Gui.drawHorizontalLine(x, x + w, y, color);
-      Gui.drawHorizontalLine(x, x + w, y + h, color);
-
-      Gui.drawVerticalLine(x, y + h, y, color);
-      Gui.drawVerticalLine(x + w, y + h, y, color);
-
-  }
